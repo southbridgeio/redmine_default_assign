56263a48989835b05730c109e1d097dc304a5973
Anastasiya Novikova <annovikova18@yandex.ru>
2024-04-24 01:03:47 +0400

    add compatibility with Redmine 5.1

--- lib/default_assign/hooks/default_assign_issues_hooks.rb
+++ lib/default_assign/hooks/default_assign_issues_hooks.rb
@@ -1,27 +1,31 @@
-class DefaultAssignIssueHook < Redmine::Hook::ViewListener
-  def view_issues_form_details_top(context = {})
-    # We only want to modify new issues; new issues haven't been assigned an id
-    return  if not context[:issue].id.nil?
+module DefaultAssign
+  module Hooks
+    class DefaultAssignIssuesHooks < Redmine::Hook::ViewListener
+      def view_issues_form_details_top(context = {})
+        # We only want to modify new issues; new issues haven't been assigned an id
+        return if not context[:issue].id.nil?
 
-    # Don't do anything if we don't want interactive assignment
-    interactive_assignment =
-      Setting.plugin_redmine_default_assign['interactive_assignment'] || 'true'
-    interactive_assignment = (interactive_assignment == 'true')
-    return  if not interactive_assignment
+        # Don't do anything if we don't want interactive assignment
+        interactive_assignment =
+          Setting.plugin_redmine_default_assign['interactive_assignment'] || 'true'
+        interactive_assignment = (interactive_assignment == 'true')
+        return if not interactive_assignment
 
-    project = context[:project]
-    if project.try(:default_assignee).blank?
-      self_assignment =
-        Setting.plugin_redmine_default_assign['self_assignment'] || 'false'
-      self_assignment = (self_assignment == 'true')
-      default_assignee = User.current  if self_assignment
-    else
-      default_assignee = project.default_assignee
+        project = context[:project]
+        if project.try(:default_assignee).blank?
+          self_assignment =
+            Setting.plugin_redmine_default_assign['self_assignment'] || 'false'
+          self_assignment = (self_assignment == 'true')
+          default_assignee = User.current if self_assignment
+        else
+          default_assignee = project.default_assignee
+        end
+        if project && project.assignable_users.include?(default_assignee)
+          context[:issue].assigned_to_id = default_assignee.id
+        end
+
+        nil
+      end
     end
-    if project && project.assignable_users.include?(default_assignee)
-      context[:issue].assigned_to_id = default_assignee.id
-    end
-
-    nil
   end
 end
